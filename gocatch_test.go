5c3d03f69b9e6b93e96f63677bf5b95fd5fd0544
Yang Wang <357466524@qq.com>
2019-05-01 23:52:04 +0800

    Test cover 100%

--- gocatch_test.go
+++ gocatch_test.go
@@ -14,10 +14,42 @@ func TestCatch(t *testing.T){
 	a := 100 / z
 	t.Errorf("%v", a)
 }
+func TestCatchReturnNull(t *testing.T){
+	fmt.Println("test 1")
+	defer Catch(nil)
+	z := 0
+	a := 100 / z
+	t.Errorf("%v", a)
+}
+func TestCatchCallbackError(t *testing.T){
+	fmt.Println("test 1")
+	defer Catch(func(i interface{}) {
+		panic("internal")
+	})
+	z := 0
+	a := 100 / z
+	t.Errorf("%v", a)
+}
 func TestCatchWithStack(t *testing.T){
 	fmt.Println("test 2")
 	defer CatchWithStack(func(e interface{}){
 		fmt.Printf("%v \n", e)
+	})
+	z := 0
+	a := 100 / z
+	t.Errorf("%v", a)
+}
+func TestCatchWithStackReturnNull(t *testing.T){
+	fmt.Println("test 2")
+	defer CatchWithStack(nil)
+	z := 0
+	a := 100 / z
+	t.Errorf("%v", a)
+}
+func TestCatchWithStackCallbackError(t *testing.T){
+	fmt.Println("test 2")
+	defer CatchWithStack(func(i interface{}) {
+		panic("internal")
 	})
 	z := 0
 	a := 100 / z
@@ -28,6 +60,22 @@ func TestCatchStack(t *testing.T){
 	defer CatchStack(func(e interface{}, stack string){
 		fmt.Printf("%v \n", e)
 		fmt.Printf("%v \n", stack)
+	})
+	z := 0
+	a := 100 / z
+	t.Errorf("%v", a)
+}
+func TestCatchStackReturnNull(t *testing.T){
+	fmt.Println("test 3")
+	defer CatchStack(nil)
+	z := 0
+	a := 100 / z
+	t.Errorf("%v", a)
+}
+func TestCatchStackCallbackError(t *testing.T){
+	fmt.Println("test 3")
+	defer CatchStack(func(i interface{}, stack string) {
+		panic("internal")
 	})
 	z := 0
 	a := 100 / z
